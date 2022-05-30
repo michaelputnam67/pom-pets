3eaede4ba57c00d3eda9357e6f46c2840dfbef1d
Ross Landino <mrlandino@gmail.com>
2022-05-30 13:02:30 -0600

    Nav bar icon display with functionality to switch pages

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProjectsPage from './Components/ProjectsPage';
-import MaterialCommunityIcons from './node_modules/react-native-vector-icons/MaterialCommunityIcons'
+import { View, Image, Text} from 'react-native'
 
 
 export default function App() {
@@ -10,13 +10,49 @@ export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Projects" component= {ProjectsPage} options= { {
-          tabBarLabel: "Projects",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          )
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel:false,
+          tabBarStyle: {
+            position: 'absolute',
+            bottom: 25,
+            left: 20,
+            right: 20,
+            elevation: 0,
+            backgroundColor: 'grey',
+            borderRadius: 10,
+            height: 90,
           }
+        }}>
+        <Tab.Screen name="Projects" component= {ProjectsPage} options= { {
+          tabBarIcon: ({focused}) => (
+           <View style={{ alignItems: 'center', justifyContent: 'center', top: 10}}>
+             <Image 
+                source={require('./assets/Icons-Buttons/PetIcon-Active.png')}
+                resizeMode='contain'
+                style={{
+                  width: 70,
+                  height: 70,
+                  tintColor: focused ? 'red' : 'black',
+                }}
+                />
+           </View>
+          )}
+        } />
+        <Tab.Screen name="User" component= {ProjectsPage} options= { {
+          tabBarIcon: ({focused}) => (
+           <View style={{ alignItems: 'center', justifyContent: 'center', top: 10}}>
+             <Image 
+                source={require('./assets/Icons-Buttons/Profile-Active.png')}
+                resizeMode='contain'
+                style={{
+                  width: 70,
+                  height: 70,
+                  tintColor: focused ? 'red' : 'black',
+                }}
+                />
+           </View>
+          )}
         } />
       </Tab.Navigator>
     </NavigationContainer>
