1e20db0a01d4332473117bf9a0949518a541efbc
Ross Landino <mrlandino@gmail.com>
2022-05-31 11:24:38 -0600

    Build Timer function

--- screens/ProjectTimerScreen.tsx
+++ screens/ProjectTimerScreen.tsx
@@ -1,7 +1,55 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Text, View, Button } from "react-native";
+import React, { useEffect, useState} from "react";
 import { SafeAreaView } from "react-native";
+import BackgroundTimer from 'react-native-background-timer';
 
 export default function ProjectTimer() {
-  return <SafeAreaView></SafeAreaView>;
+  const [secondsLeft, setSecondsLeft] = useState(3601);
+  const [timerOn, setTimerOn] = useState(false);
+
+  useEffect(() => {
+    if(timerOn) startTimer();
+    else BackgroundTimer.stopBackgroundTimer();
+    
+    return () => {
+      BackgroundTimer.stopBackGroundTimer();
+    };
+  }, [timerOn]);
+
+  useEffect (() => {
+    if (secondsLeft === 0) {
+      BackgroundTimer.stopBackgroundTimer();
+    }
+  }, [secondsLeft])
+  
+  const startTimer = () => {
+    BackgroundTimer.runBackgroundTimer(() => {
+      setSecondsLeft(seconds => {
+        if(seconds > 0) return seconds - 1;
+        else return 0;
+      });
+    }, 1000)
+  }
+  const clockify = () => {
+    let hours = Math.floor(secondsLeft / 60 / 60)
+    let mins = Math.floor(secondsLeft / 60 % 60)
+    let seconds = Math.floor(secondsLeft % 60)
+
+    let displayHours = hours < 10 ? `0${hours}` : hours;
+    let displayMins = mins < 10 ? `0${mins}` : mins;
+    let displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return {
+      displayHours,
+      displayMins,
+      displaySeconds
+    }
+  }
+  return (
+  <SafeAreaView>
+    <Text>{clockify().displayHours} Hours {clockify().displayMins} Mins {' '} {clockify().displaySeconds} Secs 
+    </Text>
+    <Button title="Start/Stop"></Button>
+  </SafeAreaView>
+  )
 }
