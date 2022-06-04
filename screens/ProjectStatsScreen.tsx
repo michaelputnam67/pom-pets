53b78c5f5f5cb7a02fa0d7a3b0d086a8c75610f5
Michael Putnam <mikeputnam713@gmail.com>
2022-06-04 09:57:06 -0700

    link project statistics screen with user stats

--- screens/ProjectStatsScreen.tsx
+++ screens/ProjectStatsScreen.tsx
@@ -2,57 +2,71 @@ import { View, SafeAreaView, StyleSheet, Text, Image } from "react-native";
 import React from "react";
 import { COLORS } from "../constants/Colors";
 import { Project } from "../interface";
-import Button from '../Components/Button';
-import HealthIcons from '../Components/HealthIcons';
-import RenderTime from '../Components/RenderTime';
+import Button from "../Components/Button";
+import HealthIcons from "../Components/HealthIcons";
+import RenderTime from "../Components/RenderTime";
+import * as Linking from "expo-linking";
 
 export default function ProjectStatisticsScreen({
   currentProject,
-	navigation
+  navigation,
 }: {
-	navigation : any;
+  navigation: any;
   currentProject: Project | undefined;
 }) {
-	const toTraining = () => {
-		navigation.navigate("Pet")
-	}
+  const toTraining = () => {
+    navigation.navigate("Pet");
+  };
 
   return (
     <SafeAreaView>
-      <Text style={styles.header}>Project Statistics</Text>	
+      <Text style={styles.header}>Project Statistics</Text>
       <Image
         style={styles.pet}
         source={
-					currentProject?.petImage === "tomato-image"
-					? require("../assets/Pets/TomatoPet.png")
-					: require("../assets/Pets/PigeonPet.png")
-				}
+          currentProject?.petImage === "tomato-image"
+            ? require("../assets/Pets/TomatoPet.png")
+            : require("../assets/Pets/PigeonPet.png")
+        }
       />
-			<Text>{currentProject?.projectName}</Text>
-			<Button text="to training" onPress={toTraining}></Button>
-			<View>
-				<Text>Level {currentProject?.petLevel}</Text>
-			</View>
-			<View>
-				<Text>Health</Text>
-				<HealthIcons health={currentProject?.petHealth} />
-			</View>
-			<View>
-				<RenderTime time={currentProject?.stats.totalWorkTime} />
-				<Text>total training time</Text>
-			</View>
-			<View>
-				<RenderTime time={currentProject?.stats.totalLongPomTime} />
-				<Text>total long pom time</Text>
-			</View>
-			<View>
-				<RenderTime time={currentProject?.stats.totalShortPomTime} />
-				<Text>total short pom time</Text>
-			</View>
-			<View>
-				<RenderTime time={currentProject?.stats.totalWorkSessions} />
-				<Text>number of work sessions</Text>
-			</View>
+      <Text>Project Name: {currentProject?.projectName}</Text>
+      <Button text="to training" onPress={toTraining}></Button>
+      <View>
+        <Text>Level {currentProject?.petLevel}</Text>
+      </View>
+      <View>
+        <Text>Health</Text>
+        <HealthIcons health={currentProject?.petHealth} />
+      </View>
+      <View>
+        <RenderTime time={currentProject?.stats.totalWorkTime} />
+        <Text>total training time</Text>
+      </View>
+      <View>
+        <RenderTime time={currentProject?.stats.totalLongPomTime} />
+        <Text>total long pom time</Text>
+      </View>
+      <View>
+        <RenderTime time={currentProject?.stats.totalShortPomTime} />
+        <Text>total short pom time</Text>
+      </View>
+      <View>
+        <Text>{currentProject?.stats.totalWorkSessions}</Text>
+        <Text>number of work sessions</Text>
+      </View>
+      <View>
+        <Text>{currentProject?.stats.totalShortSessions}</Text>
+        <Text>number of short breaks</Text>
+      </View>
+      <View>
+        <Text
+          onPress={() => Linking.openURL(`${currentProject?.projectGitHub}`)}
+        >
+          {" "}
+          {currentProject?.projectGitHub}{" "}
+        </Text>
+        <Text>Git Hub Project Link</Text>
+      </View>
     </SafeAreaView>
   );
 }
